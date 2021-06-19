5bb7798fe595e8f80887188dcccbeb3585db2193
mm12583 <lordarcher12345@gmail.com>
2021-06-19 14:09:13 +0800

    modife app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const number = document.getElementById("number");
+const btn = document.getElementById("btn");
+// console.log(number, btn);
+
+let randomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber);
+btn.addEventListener("click", function (e) {
+  const userVal = parseInt(number.value, 10); // 轉成10進位整數
+  //   console.log(userVal, randomNumber);
+  if (userVal === randomNumber) {
+    alert("Your are right");
+    return;
+  } else if (userVal > randomNumber) {
+    alert("smaller");
+    return;
+  } else {
+    alert("larger");
+    return;
+  }
+});
